a95ea53b7e659db77b3616bfb9c01d280658d81f
Mark <mark@Marks-MacBook-Pro.local>
2015-08-20 20:46:50 +0400

    WIP

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -6,74 +6,80 @@
 
 	<div class="container">
 		<div class="row">
-			<div class="col-md-6 col-md-offset-3">
-				<h2 class="text-center">Signup for a new account</h2>
+			<div class="col s12 m6 offset-m3">
+				
+				<h2>Signup for a new account</h2>
+
 				{!! Form::open() !!}
-				{!! Form::hidden('type', 'freelancer') !!}
-				<div class="form-group">
-					{!! Form::label('email') !!}
-					{!! Form::email('email', null, ['class' => 'form-control']) !!}
-				</div>
-				<div class="form-group">
+
+					{!! Form::hidden('type', 'freelancer') !!}
+
 					<div class="row">
-						<div class="col-md-6">
-							{!! Form::label('password') !!}
-							{!! Form::password('password', ['class' => 'form-control']) !!}
+
+						<div class="input-field col s12 m12">
+							{!! Form::label('email') !!}
+							{!! Form::email('email', null, ['class' => 'validate']) !!}
+						</div>
+					
+						<div class="input-field col s12 m12">
+							<div class="row">
+								<div class="input-field col s12 m6">
+									{!! Form::password('password', ['class' => 'validate']) !!}
+									{!! Form::label('password') !!}
+								</div>
+
+								<div class="input-field col s12 m6">
+									{!! Form::password('password_confirmation', ['class' => 'validate']) !!}
+									{!! Form::label('password_confirmation') !!}
+								</div>
+							</div>
 						</div>
 
-						<div class="col-md-6">
-							{!! Form::label('password_confirmation') !!}
-							{!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
+						<div class="input-field col s12 m12">
+							<div class="row">
+								<div class="input-field col s12 m6">
+									{!! Form::label('firstName', 'First Name') !!}
+									{!! Form::text('firstName', null, ['class' => 'validate']) !!}
+								</div>
+								<div class="input-field col s12 m6">
+									{!! Form::label('lastName', 'Last Name') !!}
+									{!! Form::text('lastName', null, ['class' => 'validate']) !!}
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
 
-				<div class="form-group">
-					<div class="row">
-						<div class="col-md-6">
-							{!! Form::label('firstName', 'First Name') !!}
-							{!! Form::text('firstName', null, ['class' => 'form-control']) !!}
+						<div class="input-field col s12 m12">
+							{!! Form::label('country') !!}
+							{!! Form::select('country', $countries->all(), null, ['class' => 'validate']) !!}
 						</div>
-						<div class="col-md-6">
-							{!! Form::label('lastName', 'Last Name') !!}
-							{!! Form::text('lastName', null, ['class' => 'form-control']) !!}
+
+						<div class="input-field col s12 m12">
+							{!! Form::label('phoneNumber', 'Phone Number') !!}
+							{!! Form::text('phoneNumber', null, ['class' => 'validate']) !!}
 						</div>
-					</div>
-				</div>
+							
 
-				<div class="form-group">
-					{!! Form::label('country') !!}
-					{!! Form::select('country', $countries->all(), null, ['class' => 'form-control']) !!}
-				</div>
+						<div class="col s12 m12">
+							{!! Form::checkbox('terms', 1, 0, ['id' => 'filled-in-box', 'class' => 'filled-in']) !!}
+							<label for="filled-in-box">I have read and understood the <a href="#">Terms & Conditions</a>.</label>
+							
+						</div>
 
-				<div class="form-group">
-					{!! Form::label('phoneNumber', 'Phone Number') !!}
-					{!! Form::text('phoneNumber', null, ['class' => 'form-control']) !!}
-				</div>
-				
-				<hr />			
+						<div class="input-field col s12 m12">
+							<p class="text-muted">We take your privacy very seriously and we do not use, sell or share your personal information for any reason at all, EVER!</p>
+						</div>
 
-				<div class="checkbox">
-					<label>
-						{!! Form::checkbox('terms', 1, 0) !!} I have read and understood the <a href="#">Terms & Conditions</a>.
-					</label>
-				</div>
+						
+						<div class="input-field col s12 m12">
+							<div class="row">
+								<div class="col s12 m4 right">
+									<button type="submit" name="submit" class="btn waves-effect waves-light btn-large">Sign up</button>
+								</div>	
+							</div>
+						</div>
 
-				<div class="form-group">
-					<p class="text-muted">We take your privacy very seriously and we do not use, sell or share your personal information for any reason at all, EVER!</p>
-				</div>
-
-				
-
-				<div class="form-group">
-					<div class="row">
-						<div class="col-md-4 float-right">
-							{!! Form::submit('Sign up',['class' => 'btn btn-success btn-lg btn-block']) !!}
-						</div>	
-					</div>
-				</div>
-
-				@include('partials.errors')			
+						@include('partials.errors')	
+					</div>		
 
 				{!! Form::close() !!}
 			</div>
--- resources/views/partials/topnav.blade.php
+++ resources/views/partials/topnav.blade.php
@@ -1,35 +1,37 @@
-
-<nav class="top-nav">
-	<div class="container">
-		<div class="nav-wrapper">
-			@if( Auth::check() )
-				<a class="brand-logo" href="{{ route('profile.index') }}">Paul</a>
-			@else
-				<a class="brand-logo" href="/">Paul</a>
-			@endif	
-			<ul id="nav-mobile" class="right hide-on-med-and-down">
-				<li><a href="#">Helpdesk</a></li>
-				<li><a href="#">Browse Jobs</a></li>
-				<li><a href="#">Blog</a></li>
+<div class="navbar-fixed">
+	<nav class="top-nav">
+		<div class="container">
+			<div class="nav-wrapper">
 				@if( Auth::check() )
-				<li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ $currentUser->fullName }} <span class="caret"></span></a>
-					<ul class="dropdown-menu">
-						<li><a href="#">My Profile</a></li>
-						<li><a href="#">Manage Jobs</a></li>
-						<li><a href="#">My Applications</a></li>
-						<li><a href="#">Notifications</a></li>
-						<li><a href="#">Account Settings</a></li>
-						<li role="separator" class="divider"></li>
-						<li><a href="/auth/logout">Log out</a></li>
-					</ul>
-				</li>				
-				
+					<a class="brand-logo" href="{{ route('profile.index') }}">Paul</a>
 				@else
-				<li><a href="/auth/register">Sign up</a></li>
-				<li><a href="/auth/login">Log in</a></li>				
-				@endif
-			</ul>
+					<a class="brand-logo" href="/">Paul</a>
+				@endif	
+				<ul id="nav-mobile" class="right hide-on-med-and-down">
+					<li><a href="#">Helpdesk</a></li>
+					<li><a href="#">Browse Jobs</a></li>
+					<li><a href="#">Blog</a></li>
+					@if( Auth::check() )
+					<li>
+						<a href="#" class="dropdown-button" data-activates="userProfileDropdown">{{ $currentUser->fullName }} <span class="caret"></span></a>
+					</li>				
+					
+					@else
+					<li><a href="/auth/register">Sign up</a></li>
+					<li><a href="/auth/login">Log in</a></li>				
+					@endif
+				</ul>
+			</div>
 		</div>
-	</div>
-</nav>
+	</nav>
+</div>
+
+<ul class="dropdown-content" id="userProfileDropdown">
+	<li><a href="#">My Profile</a></li>
+	<li><a href="#">Manage Jobs</a></li>
+	<li><a href="#">My Applications</a></li>
+	<li><a href="#">Notifications</a></li>
+	<li><a href="#">Account Settings</a></li>
+	<li role="separator" class="divider"></li>
+	<li><a href="/auth/logout">Log out</a></li>
+</ul>
